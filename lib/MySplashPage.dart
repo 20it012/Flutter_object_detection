c9235ebdef04f2592f3c7f54597ec19c2769d73c
unknown <20it012@charusat.edu.in>
2023-03-31 13:36:51 +0530

    add splashscreen

--- lib/MySplashPage.dart
+++ lib/MySplashPage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:splashscreen/splashscreen.dart';
 
 class MySplashPage extends StatefulWidget {
   const MySplashPage({Key? key}) : super(key: key);
@@ -10,6 +11,13 @@ class MySplashPage extends StatefulWidget {
 class _MySplashPageState extends State<MySplashPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SplashScreen(
+      seconds: 12,
+      // navigateAfterSeconds: ,
+      imageBackground: Image.asset("assets/back.jpg").image,
+      useLoader: true,
+      loaderColor: Colors.pink,
+      loadingText: const Text("loading...", style: TextStyle(color: Colors.white),),
+    );
   }
 }
