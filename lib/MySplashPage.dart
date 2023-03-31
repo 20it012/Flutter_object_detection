192fd5f0bd8ee60448746d694840bf980b903d78
unknown <20it012@charusat.edu.in>
2023-03-31 13:19:51 +0530

    initial and first commit

---
+++ lib/MySplashPage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MySplashPage extends StatefulWidget {
+  const MySplashPage({Key? key}) : super(key: key);
+
+  @override
+  State<MySplashPage> createState() => _MySplashPageState();
+}
+
+class _MySplashPageState extends State<MySplashPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
