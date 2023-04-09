41facd99ff31eba94f198d3e1c6b7b4510b42111
unknown <20it012@charusat.edu.in>
2023-04-09 16:59:38 +0530

    dependancy correct

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -1,10 +1,12 @@
-// import 'package:camera/camera.dart';
+import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_object_detection/main.dart';
+import 'package:flutter_tflite/flutter_tflite.dart';
+
+
 
 class HomePage extends StatefulWidget {
-  const HomePage({Key? key}) : super(key: key);
-
+  // const HomePage({Key? key}) : super(key: key);
   @override
   State<HomePage> createState() => _HomePageState();
 }
@@ -13,11 +15,38 @@ class _HomePageState extends State<HomePage>
 {
   bool isWorking = false;
   String result="";
-  CameraController cameraController;
-  CameraImage imgCamera;
+  late CameraController cameraController;
+  CameraImage? imgCamera;
+  //
+  // List<CameraDescription>? cameras;
+  // late CameraController controller;
+
+  // Future<void> setupCamera() async {
+  //   try {
+  //     // Get available cameras
+  //     cameras = await availableCameras();
+  //     // Initialize the camera controller
+  //     cameraController = CameraController(cameras![0], ResolutionPreset.max);
+  //     // Initialize the camera controller
+  //     await cameraController.initialize();
+  //     // Start camera preview
+  //     await cameraController.startImageStream(onCameraImage);
+  //     print('Camera Initialized Successfully');
+  //   } catch (e) {
+  //     print('Failed to set up camera: $e');
+  //   }
+  // }
+
+  // loadModel() async
+  // {
+  //   await Tflite.loadModel(
+  //     model: "mobilenet_v1_1.0_224.tflite",
+  //     labels: "mobilenet_v1_1.0_224.txt",
+  //   );
+  // }
 
   initCamera(){
-    cameraController = CameraController(cameras[0], ResolutionPreset.medium);
+    cameraController = CameraController(cameras![0], ResolutionPreset.medium);
     cameraController.initialize().then((value)
     {
       if(!mounted)
@@ -25,13 +54,14 @@ class _HomePageState extends State<HomePage>
           return;
         }
 
-      setState(() {
-        cameraController.startImageStream((imageFromStream) =>
+        setState(() {
+          cameraController.startImageStream((imageFromStream) =>
         {
           if(!isWorking)
           {
             isWorking = true,
             imgCamera = imageFromStream,
+            // runModelOnStreamFrames();
           }
 
         });
@@ -39,55 +69,98 @@ class _HomePageState extends State<HomePage>
     });
   }
 
+  // runModelOnStreamFrames() async
+  // {
+  //   if(imgCamera != null)
+  //     {
+  //       var recognitions = await Tflite.runModelOnFrame(
+  //           bytesList: imgCamera.planes.map((plane)
+  //           {
+  //             return plane.bytes;
+  //           }).toList(),
+  //
+  //           imageHeight: imgCamera.height,
+  //           imageWidth: imgCamera.width,
+  //           imageMean: 127.5,
+  //           imageStd: 127.5,
+  //           rotation: 90,
+  //           numResults: 2,
+  //           threshold: 0.1,
+  //           asynch: true,
+  //           );
+  //       result = "";
+  //       recognitions.forEach((response)
+  //       {
+  //         result += response["label"] + "" + (response["confidence"] as double).toStringAsFixed(2) + "\n \n";
+  //       });
+  //
+  //       setState(() {
+  //         result;
+  //       });
+  //
+  //       isWorking = false;
+  //     }
+  // }
+  //
+  // @override
+  // void initState() {
+  //   super.initState();
+  //
+  //   loadModel();
+  // }
+  //
+  // @override
+  // void dispose() async
+  // {
+  //   super.dispose();
+  //
+  //   await Tflite.close();
+  //   cameraController?.dispose();
+  // }
+
   @override
   Widget build(BuildContext context)
   {
-    return MaterialApp(
-      home: SafeArea(
-        child: Scaffold(
+    return Scaffold(
           body: Container(
             decoration: BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage("assets/jarvis.jpg")
-              )
+                image: DecorationImage(
+                    image: AssetImage("assets/jarvis.jpg")
+                ),
             ),
             child: Column(
               children: [
                 Center(
                   child: Container(
+                    color: Colors.black,
                     height: 320,
                     width: 330,
                     child: Image.asset("assets/camera.jpg"),
                   ),
                 ),
                 Center(
-                  child: FlatButton(
-                    onPressed: ()
-                    {
-                      initCamera();
-                    },
+                  child: FloatingActionButton(
+                    onPressed: initCamera(),
                     child: Container(
                       margin: EdgeInsets.only(top: 35),
                       height: 270,
                       width: 305,
                       child: imgCamera == null
                           ? Container(
-                            height: 270,
-                            width: 360,
-                            child: Icon(Icons.photo_camera_front, color: Colors.blueAccent, size: 40,),
+                              height: 270,
+                              width: 360,
+                              child: Icon(Icons.photo_camera_front, color: Colors.blueAccent, size: 40,),
                             )
                           : AspectRatio(
-                            aspectRatio: cameraController.value.aspectRatio,
-                            child: CameraPreview(cameraController),
-                        );
+                              aspectRatio: cameraController.value.aspectRatio,
+                              child: CameraPreview(cameraController),
+                            ),
                     ),
                   ),
                 ),
               ],
             ),
           ),
-        ),
-      ),
     );
   }
 }
--- lib/MySplashPage.dart
+++ lib/MySplashPage.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_object_detection/HomePage.dart';
-import 'package:splashscreen/splashscreen.dart';
+import 'package:animated_splash_screen/animated_splash_screen.dart';
+// import 'package:page_transition/page_transition.dart';
 
 class MySplashPage extends StatefulWidget {
-  const MySplashPage({Key? key}) : super(key: key);
+  // const MySplashPage({Key? key}) : super(key: key);
 
   @override
   State<MySplashPage> createState() => _MySplashPageState();
@@ -12,13 +13,22 @@ class MySplashPage extends StatefulWidget {
 class _MySplashPageState extends State<MySplashPage> {
   @override
   Widget build(BuildContext context) {
-    return SplashScreen(
-      seconds: 12,
-      navigateAfterSeconds: HomePage(),
-      imageBackground: Image.asset("assets/back.jpg").image,
-      useLoader: true,
-      loaderColor: Colors.pink,
-      loadingText: const Text("loading...", style: TextStyle(color: Colors.white),),
+    return AnimatedSplashScreen(
+      duration: 3000,
+      nextScreen: HomePage(),
+      splash: Image.asset("assets/back.jpg"),
+        // splashIconSize: 9000,
+        // splashTransition: SplashTransition.fadeTransition,
+        // pageTransitionType: PageTransitionType.scale,
+        // backgroundColor: Colors.blue
+      // fit: BoxFit.cover,
+      // height: double.infinity,
+      // width: double.infinity,
+      // alignment: Alignment.center,
+      //
+      // useLoader: true,
+      // loaderColor: Colors.pink,
+      // loadingText: const Text("loading...", style: TextStyle(color: Colors.white),),
     );
   }
 }
